7e96e620bc883f6405c8c37d24de0dd490a0c017
ohanke <steamoha@wp.pl>
2022-06-05 19:52:32 +0200

    Lecture 3. find max/min value.

---
+++ src/main/java/com/oscarhanke/lectures/Lecture3.java
@@ -0,0 +1,27 @@
+package com.oscarhanke.lectures;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class Lecture3 {
+    public static void main(String[] args) {
+        final List<Integer> numbers = List.of(1, 2, 3, 100, 23, 93, 99);
+        System.out.println(getMinValue(numbers));
+        System.out.println(getMaxValue(numbers));
+    }
+
+    public static int getMinValue(List<Integer> numbers){
+        return numbers.stream()
+                .min(Comparator.naturalOrder())
+                //.min((x, y) -> x > y ? 1 : 0)
+                .get();
+    }
+
+    public static int getMaxValue(List<Integer> numbers){
+        return numbers.stream()
+                .max(Comparator.naturalOrder())
+                .get();
+    }
+}
+
+
