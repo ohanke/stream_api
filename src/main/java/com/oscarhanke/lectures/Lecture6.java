2126334e31ae77fa226d940deccdef2184bba7fa
ohanke <steamoha@wp.pl>
2022-06-05 21:09:38 +0200

    Lecture 6. Find first.

--- src/main/java/com/oscarhanke/lectures/Lecture6.java
+++ src/main/java/com/oscarhanke/lectures/Lecture6.java
@@ -9,13 +9,23 @@ import java.util.List;
 public class Lecture6 {
     public static void main(String[] args) {
         Integer [] numbers = {1,2,3,4,5,6,7,8,9,10};
-        Integer number = findAnyLowerThanTen(numbers);
-        System.out.println(number);
+        Integer anyNumber = findAnyLowerThanTen(numbers);
+        System.out.println("Find any: " + anyNumber);
+        Integer firstNumber = findFirstLowerThanTen(numbers);
+        System.out.println("Find first: " + firstNumber);
+    }
+
+    private static Integer findFirstLowerThanTen(Integer [] numbers) {
+        return Arrays.stream(numbers)
+                .filter(number -> number < 10)
+                .findFirst()
+                .orElse(0);
     }
 
     public static Integer findAnyLowerThanTen(Integer [] numbers){
         return Arrays.stream(numbers)
                 .filter(number -> number < 10)
-                .findAny().orElse(0);
+                .findAny()
+                .orElse(0);
     }
 }
