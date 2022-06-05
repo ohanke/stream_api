97ce19354615fad471b1f9f53146fee76d985de0
ohanke <steamoha@wp.pl>
2022-06-05 20:04:16 +0200

    Lecture 4. Distinct values through Set.

--- src/main/java/com/oscarhanke/lectures/Lecture4.java
+++ src/main/java/com/oscarhanke/lectures/Lecture4.java
@@ -1,15 +1,33 @@
 package com.oscarhanke.lectures;
 
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class Lecture4 {
     public static void main(String[] args) {
         final List<Integer> numbers = List.of(1,1,1,2,2,3,3,3,3,3,4,4,4,4,5,5,5,5,6,6,6,6,7,7,7,8,9);
 
-        List<Integer> distincNumbers = numbers.stream()
+        List<Integer> distincNumbersList = getDistincValuesList(numbers);
+        System.out.println("");
+        System.out.println("distincNumbersList :");
+        distincNumbersList.forEach(System.out::println);
+
+        Set<Integer> numberSet = getNumberSet(numbers);
+        System.out.println("");
+        System.out.println("NumberSet: ");
+        numberSet.forEach(System.out::println);
+
+    }
+
+    public static List<Integer> getDistincValuesList(List<Integer> numbers){
+        return numbers.stream()
                 .distinct()
                 .collect(Collectors.toList());
-        System.out.println("List size: " + distincNumbers.size());
+    }
+
+    public static Set<Integer> getNumberSet(List<Integer> numbers){
+        return numbers.stream()
+                .collect(Collectors.toSet());
     }
 }
