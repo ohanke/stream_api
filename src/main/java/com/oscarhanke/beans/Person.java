929a1eeb89e77d85c0ba1aa30631f553bbd5f9c1
ohanke <steamoha@wp.pl>
2022-06-05 18:09:30 +0200

    Lecture 1. stream.filer.limit.collect

---
+++ src/main/java/com/oscarhanke/beans/Person.java
@@ -0,0 +1,56 @@
+package com.oscarhanke.beans;
+
+public class Person {
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String gender;
+    private Integer age;
+
+    public Person(Integer id, String firstName, String lastName, String email, String gender, Integer age) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.gender = gender;
+        this.age = age;
+    }
+
+    public Person(String firstName, Integer age) {
+        this.firstName = firstName;
+        this.age = age;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "firstName='" + firstName + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
