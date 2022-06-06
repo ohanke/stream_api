e4df8ff7eeee5eb0fff262ed802e6307bd71ae36
ohanke <steamoha@wp.pl>
2022-06-06 12:15:20 +0200

    Lecture 8. GroupByName and count.

--- src/main/java/com/oscarhanke/lectures/Lecture8GroupBy.java
+++ src/main/java/com/oscarhanke/lectures/Lecture8GroupBy.java
@@ -5,6 +5,7 @@ import com.oscarhanke.beans.Person;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class Lecture8GroupBy {
@@ -16,6 +17,18 @@ public class Lecture8GroupBy {
             System.out.println(age);
             peoples.forEach(System.out::println);
         });
+
+        System.out.println("");
+        System.out.println("GroupingAndCounting: ");
+        Map<String, Long> groupingAndCounting = getGroupingAndCounting(people);
+        groupingAndCounting.forEach((name, count) -> System.out.println(name + " > " + count));
+
+    }
+
+    private static Map<String, Long> getGroupingAndCounting(List<Person> people){
+        return people.stream()
+//                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+                .collect(Collectors.groupingBy(Person::getFirstName, Collectors.counting()));
     }
 
     private static Map<Integer, List<Person>> getSimpleGrouping(List<Person> people) {
@@ -24,7 +37,7 @@ public class Lecture8GroupBy {
     }
 
     public static List<Person> getPeoples(){
-        String [] names = {"One","One","One","One","Five","Six","Seven","Eight","Nine","Ten"};
+        String [] names = {"One","One","One","One","Five","Six","Seven","Seven","Nine","Ten"};
         Integer [] age = {3, 2, 5, 2, 2, 6, 7, 8, 9, 10};
         int peoplesAmount = 10;
         List<Person> peoples = new ArrayList<>();
